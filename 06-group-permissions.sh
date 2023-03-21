d6240cb6496005d5eba8a73e8938dc0f402cb0fe
cloud-jake <jake.holmquist@kinandcarta.com>
2023-03-21 15:50:23 -0400

    Create Groups

--- 06-group-permissions.sh
+++ 06-group-permissions.sh
@@ -2,6 +2,10 @@
 
 # Assign Permissions to retail groups
 #
+# Create Groups
+#
+#   gcloud identity groups create ${GROUP_EMAIL} --organization=${ORGANIZATION_ID} 
+#
 # https://cloud.google.com/retail/docs/iam
 #
 # Retail roles require the following GCP permissions:
@@ -11,7 +15,7 @@
 #  - serviceusage.services.get
 #
 # Apply the Service Usage Viewer role roles/serviceusage.serviceUsageViewer
-
+#
 
 source variables.env
 randomnumber=`cat random.tmp`
@@ -20,12 +24,19 @@ randomnumber=`cat random.tmp`
 #"${PRJPRE}-${PRJD}-retail-api-${randomnumber}"
 #"${PRJPRE}-${PRJT}-retail-api-${randomnumber}"
 
+# Set Project config variable to Common project
+# "${PRJPRE}-${PRJC}-retail-${randomnumber}"
+gcloud config set project "${PRJPRE}-${PRJC}-retail-${randomnumber}"
+
+# Enable cloudidentity.googleapis.com service on Common project
+gcloud services enable cloudidentity.googleapis.com  --project="${PRJPRE}-${PRJC}-retail-${randomnumber}"
+
 
 #[retail-admin]
 # Retail Admin Role 
 #    roles/retail.admin
 GROUP_EMAIL="retail-admin@${DOMAIN}"
-
+gcloud identity groups create ${GROUP_EMAIL} --organization=${ORGANIZATION_ID}
 PROJECT_ID="${PRJPRE}-${PRJP}-retail-api-${randomnumber}"
 gcloud projects add-iam-policy-binding "${PROJECT_ID}" --member=group:${GROUP_EMAIL} --role=roles/retail.admin
 gcloud projects add-iam-policy-binding "${PROJECT_ID}" --member=group:${GROUP_EMAIL} --role=roles/serviceusage.serviceUsageViewer
@@ -43,7 +54,7 @@ gcloud projects add-iam-policy-binding "${PROJECT_ID}" --member=group:${GROUP_EM
 # Retail Editor Role
 #    roles/retail.editor
 GROUP_EMAIL="retail-editor@${DOMAIN}"
-
+gcloud identity groups create ${GROUP_EMAIL} --organization=${ORGANIZATION_ID}
 PROJECT_ID="${PRJPRE}-${PRJP}-retail-api-${randomnumber}"
 gcloud projects add-iam-policy-binding "${PROJECT_ID}" --member=group:${GROUP_EMAIL} --role=roles/retail.editor
 gcloud projects add-iam-policy-binding "${PROJECT_ID}" --member=group:${GROUP_EMAIL} --role=roles/serviceusage.serviceUsageViewer
@@ -61,7 +72,7 @@ gcloud projects add-iam-policy-binding "${PROJECT_ID}" --member=group:${GROUP_EM
 # Retail Viewer Role
 #    roles/retail.viewer
 GROUP_EMAIL="retail-viewer@${DOMAIN}"
-
+gcloud identity groups create ${GROUP_EMAIL} --organization=${ORGANIZATION_ID}
 PROJECT_ID="${PRJPRE}-${PRJP}-retail-api-${randomnumber}"
 gcloud projects add-iam-policy-binding "${PROJECT_ID}" --member=group:${GROUP_EMAIL} --role=roles/retail.viewer
 gcloud projects add-iam-policy-binding "${PROJECT_ID}" --member=group:${GROUP_EMAIL} --role=roles/serviceusage.serviceUsageViewer
@@ -83,7 +94,7 @@ gcloud projects add-iam-policy-binding "${PROJECT_ID}" --member=group:${GROUP_EM
 #  All environments
 #    roles/retail.editor
 GROUP_EMAIL="retail-merchandising@${DOMAIN}"
-
+gcloud identity groups create ${GROUP_EMAIL} --organization=${ORGANIZATION_ID}
 #PROD
 PROJECT_ID="${PRJPRE}-${PRJP}-retail-api-${randomnumber}"
 gcloud projects add-iam-policy-binding "${PROJECT_ID}" --member=group:${GROUP_EMAIL} --role=roles/retail.editor
@@ -112,7 +123,7 @@ gcloud projects add-iam-policy-binding "${PROJECT_ID}" --member=group:${GROUP_EM
 #    roles/logging.viewer
 
 GROUP_EMAIL="retail-developer@${DOMAIN}"
-
+gcloud identity groups create ${GROUP_EMAIL} --organization=${ORGANIZATION_ID}
 #PROD
 PROJECT_ID="${PRJPRE}-${PRJP}-retail-api-${randomnumber}"
 gcloud projects add-iam-policy-binding "${PROJECT_ID}" --member=group:${GROUP_EMAIL} --role=roles/retail.viewer
@@ -147,7 +158,7 @@ gcloud projects add-iam-policy-binding "${PROJECT_ID}" --member=group:${GROUP_EM
 #    roles/logging.viewer
 
 GROUP_EMAIL="retail-operations@${DOMAIN}"
-
+gcloud identity groups create ${GROUP_EMAIL} --organization=${ORGANIZATION_ID}
 #PROD
 PROJECT_ID="${PRJPRE}-${PRJP}-retail-api-${randomnumber}"
 gcloud projects add-iam-policy-binding "${PROJECT_ID}" --member=group:${GROUP_EMAIL} --role=roles/retail.viewer
