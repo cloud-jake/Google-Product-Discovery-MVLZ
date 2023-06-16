0f7b1c2559589c9dc083d246d8c8b7f38c39b6e3
Jake Holmquist <101203373+cloud-jake@users.noreply.github.com>
2023-06-16 13:24:45 -0400

    added billing budget

--- rapid-demonstrator/01-create-project.sh
+++ rapid-demonstrator/01-create-project.sh
@@ -11,3 +11,16 @@ source variables.env
 # Create Project - 6-30 characters
 gcloud projects create "${PROJECT_ID}" --folder=$FOLDER_ID
 gcloud beta billing projects link "${PROJECT_ID}" --billing-account=$BILLING_ID
+
+gcloud services enable \
+       billingbudgets.googleapis.com \
+       --project="${PROJECT_ID}"
+
+gcloud billing budgets create --billing-account=$BILLING_ID \
+       --display-name="budget-${PROJECT_ID}" \
+       --budget-amount=1 \
+       --calendar-period=month \
+       --threshold-rule=percent=0.5 \
+       --threshold-rule=percent=0.75 \
+       --threshold-rule=percent=1.0,basis=current-spend \
+       --filter-projects="projects/${PROJECT_ID}"
